52a9122d9b6e47581894d45130d8e25671aee010
phamdatqn <phamdat.qn@gmail.com>
2022-10-14 13:37:12 +0700

    update ss2

--- ss02_typescript/exercise/sum_fibonacci/src/main.js
+++ ss02_typescript/exercise/sum_fibonacci/src/main.js
@@ -1,15 +1,14 @@
-function fibonacci(index) {
-    if (index < 2) {
+function fibonacci(num) {
+    if (num < 2) {
         return 1;
     }
-    return fibonacci(index - 1) + fibonacci(index - 2);
+    return fibonacci(num - 1) + fibonacci(num - 2);
 }
-var sum = 0;
 var size = 10;
 var fibo = 0;
 var total = 0;
 for (var i = 0; i < size; i++) {
-    fibo = sum + fibonacci(i);
+    fibo = fibonacci(i);
     console.log("Số fabonacci thứ " + (i + 1) + " là: " + fibo);
     total += fibo;
 }
--- ss02_typescript/exercise/sum_fibonacci/src/main.ts
+++ ss02_typescript/exercise/sum_fibonacci/src/main.ts
@@ -1,16 +1,15 @@
-function fibonacci(index: number): number {
-    if (index < 2) {
+function fibonacci(num: number): number {
+    if (num < 2) {
         return 1;
     }
-    return fibonacci(index - 1) + fibonacci(index - 2);
+    return fibonacci(num - 1) + fibonacci(num - 2);
 }
 
-let sum: number = 0;
 let size: number = 10;
 let fibo: number = 0;
 let total: number = 0;
 for (let i = 0; i < size; i++) {
-    fibo = sum + fibonacci(i);
+    fibo = fibonacci(i);
     console.log("Số fabonacci thứ " + (i + 1) + " là: " + fibo);
     total += fibo;
 }
